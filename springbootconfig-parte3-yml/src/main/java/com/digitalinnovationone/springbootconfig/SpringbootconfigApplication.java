79bef519f2179745f53a660968fe0f19f0e13d9a
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-10-01 19:03:33 -0300

    Desenvolvida parte 3 do curso: YAML files

---
+++ springbootconfig-parte3-yml/src/main/java/com/digitalinnovationone/springbootconfig/SpringbootconfigApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootconfigApplication.class, args);
+	}
+
+}
