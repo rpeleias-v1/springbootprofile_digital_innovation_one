4b09e61a80c2ac579807960ec3b624ed96dc62c0
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-10-01 18:48:56 -0300

    Criadas configurações iniciais do Exercício 1

---
+++ springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
@@ -0,0 +1,34 @@
+package com.digitalinnovationone.springbootconfig.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+        System.out.println("DB connection for MYSQL Database: production");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MySql production - Test instance";
+    }
+}
